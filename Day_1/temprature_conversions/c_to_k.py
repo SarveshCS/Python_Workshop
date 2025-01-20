1f6ac31166aff443a5fb9a810509dd4de216ef3c
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-20 13:52:49 +0530

    Addded temprature comversion programs

---
+++ Day_1/temprature_conversions/c_to_k.py
@@ -0,0 +1,5 @@
+a = int(input("Enter temprature in degree: "))
+
+k = ((9/5) * a) + 32
+
+print("Tempraature in farenheit: ", k)
---
+++ Day_1/temprature_conversions/k_to_c.py
@@ -0,0 +1,5 @@
+a = int(input("Enter temprature in farenheit: "))
+
+k = (a-32) * (5/9)
+
+print("Tempraature in celcius: ", k)
