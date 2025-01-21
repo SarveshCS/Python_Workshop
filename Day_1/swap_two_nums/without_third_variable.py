74b8628fa8b461f6d8006517b997217cc4bb552a
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-21 15:20:40 +0530

    Used shorthand for xor swapping

--- Day_1/swap_two_nums/without_third_variable.py
+++ Day_1/swap_two_nums/without_third_variable.py
@@ -3,8 +3,12 @@ b = int(input("Enter b: "))
 
 print("Before swap a, b : ", a, b)
 
-a = a ^ b
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+
+a ^= b
 b = a ^ b
-a = a ^ b
+a ^= b
 
 print("After swap a, b  : ", a, b)
